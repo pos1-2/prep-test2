9f984d1d5ca46ff96327c9ccead840e8b220ee42
Florian Hassanen <florian.hassanen@gmail.com>
2014-12-09 17:36:42 +0100

    add where is waldo excercise

--- src/pos1_2ahif/prep_test2/Main.java
+++ src/pos1_2ahif/prep_test2/Main.java
@@ -5,6 +5,23 @@ package pos1_2ahif.prep_test2;
  */
 public class Main {
     public static void main(String args[]) {
-        System.out.println("hello test!");
+        System.out.println("Waldo is hiding! Be patient...");
+        WhereIsWaldo whereIsWaldo = new WhereIsWaldo(100); // challenge: do 10000000
+        System.out.println("Waldo is ready now! Go find him!");
+        System.out.println("Searching...");
+
+
+        // CHANGE THIS LINE
+        WhereIsWaldo.Exercises solution = null; // TODO provide your implementation here
+        // CHANGE THIS LINE
+
+        int found = solution.findWaldo(whereIsWaldo);
+
+        if (whereIsWaldo.containsKey(found)
+                && (whereIsWaldo.get(found).search() instanceof WhereIsWaldo.Waldo)) {
+            System.out.println("There he is: " + found);
+        } else {
+            System.out.println("You must be mistaken: There is no Waldo at " + found + " :(");
+        }
     }
 }
---
+++ src/pos1_2ahif/prep_test2/WhereIsWaldo.java
@@ -0,0 +1,144 @@
+package pos1_2ahif.prep_test2;
+
+import java.util.*;
+
+/**
+ * Created by Florian on 09.12.2014.
+ */
+public final class WhereIsWaldo implements Map<Integer, WhereIsWaldo.HidingPlace> {
+    // you _MUST NOT_ modify this class!
+
+    public interface Exercises {
+        int findWaldo(WhereIsWaldo whereIsWaldo);
+    }
+
+    public interface HidingPlace {
+        Object search();
+    }
+
+    public interface Waldo {
+    }
+
+    public interface Hint {
+    }
+
+    public interface HintLeft extends Hint {
+    }
+
+    public interface HintRight extends Hint {
+    }
+
+    // no need to read any further
+
+    private Map<Integer, HidingPlace> map;
+
+    public WhereIsWaldo(int difficulty) {
+        final HidingPlace left = new HidingPlaceWithDelay(new HintLeft() {
+        });
+        final HidingPlace right = new HidingPlaceWithDelay(new HintRight() {
+        });
+        final HidingPlace nothing = new HidingPlaceWithDelay(new Object() {
+        });
+
+        map = new HashMap<Integer, HidingPlace>(difficulty);
+
+        Random random = new Random();
+
+        final int waldo = random.nextInt();
+
+        map.put(waldo, new HidingPlaceWithDelay(new Waldo() {
+        }));
+
+        for (int i = 0; i < difficulty; ++i) {
+            final int position = random.nextInt();
+
+            if (map.containsKey(position)) {
+                continue;
+            }
+
+            if (random.nextDouble() < 0.9) {
+                map.put(position, nothing);
+            } else {
+                map.put(position, (position > waldo) ? left : right);
+            }
+        }
+
+        map = Collections.unmodifiableMap(map);
+    }
+
+    private static class HidingPlaceWithDelay implements HidingPlace {
+        private Object object;
+
+        public HidingPlaceWithDelay(Object object) {
+            this.object = object;
+        }
+
+        public Object search() {
+            while (true) {
+                try {
+                    Thread.sleep(100);
+                    break;
+                } catch (InterruptedException e) {
+                    System.err.println("No cheating!");
+                }
+            }
+            return object;
+        }
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public boolean containsKey(Object key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        return map.containsValue(value);
+    }
+
+    public HidingPlace get(Object key) {
+        return map.get(key);
+    }
+
+    public HidingPlace put(Integer key, HidingPlace value) {
+        return map.put(key, value);
+    }
+
+    public HidingPlace remove(Object key) {
+        return map.remove(key);
+    }
+
+    public void putAll(Map<? extends Integer, ? extends HidingPlace> m) {
+        map.putAll(m);
+    }
+
+    public void clear() {
+        map.clear();
+    }
+
+    public Set<Integer> keySet() {
+        return map.keySet();
+    }
+
+    public Collection<HidingPlace> values() {
+        return map.values();
+    }
+
+    public Set<Entry<Integer, HidingPlace>> entrySet() {
+        return map.entrySet();
+    }
+
+    public boolean equals(Object o) {
+        return map.equals(o);
+    }
+
+    public int hashCode() {
+        return map.hashCode();
+    }
+}
